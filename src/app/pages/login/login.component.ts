00f1daf20f9a517ab774dbf9b46fc7bf50a1930b
Alexandra Jacob <alufori@hotmail.fr>
2024-02-14 10:53:46 +0100

    login.component.ts préparé pour envoie des informations au back

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,7 @@
+
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup } from '@angular/forms';
-import {HeaderComponent} from "../../components/header/header.component";
 
 
 @Component({
@@ -12,36 +12,86 @@ import {HeaderComponent} from "../../components/header/header.component";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  // Initialisation du formulaire avec des contrôles pour txt, email et password
   form = new FormGroup({
-    name: new FormControl(''),
+    txt: new FormControl(''),
     email: new FormControl(''),
     password: new FormControl(''),
   });
 
+
   constructor(private router: Router){}
 
 
+  // Méthode pour gérer l'inscription
   onRegister() {
-    console.log(this.form.value);
+    if (this.form.valid) { // Vérifie si le formulaire est valide
+      const signUpModel = new SignUpModel();
+      const txtControl = this.form.get('txt');
+      const emailControl = this.form.get('email');
+      const passwordControl = this.form.get('password');
+
+      // Vérifie si les contrôles existent et ont des valeurs
+      if (txtControl && emailControl && passwordControl) {
+        if (txtControl.value !== null && emailControl.value !== null && passwordControl.value !== null) {
+          signUpModel.txt = txtControl.value;
+          signUpModel.email = emailControl.value;
+          signUpModel.password = passwordControl.value;
+
+          console.log(signUpModel);
+
+          this.router.navigate(['/home']); // Navigue vers la page d'accueil
+        } else {
+          console.error('Form controls do not have values'); // Affiche une erreur si les contrôles n'ont pas de valeurs
+        }
+      } else {
+        console.error('Form controls do not exist'); // Affiche une erreur si les contrôles n'existent pas
+      }
+    } else {
+      console.error('Form is not valid'); // Affiche une erreur si le formulaire n'est pas valide
+    }
   }
 
+  // Méthode pour gérer la connexion
   onLogin() {
-    console.log(this.form.value);
-  }
+    if (this.form.valid) {
+      const loginModel = new LoginModel();
+      const emailControl = this.form.get('email');
+      const passwordControl = this.form.get('password');
 
+      // Vérifie si les contrôles existent et ont des valeurs
+      if (emailControl && passwordControl) {
+        if (emailControl.value !== null && passwordControl.value !== null) {
+          loginModel.email = emailControl.value;
+          loginModel.password = passwordControl.value;
+
+          console.log(loginModel);
+
+          this.router.navigate(['/home']);
+        } else {
+          console.error('Form controls do not have values'); // Affiche une erreur si les contrôles n'ont pas de valeurs
+        }
+      } else {
+        console.error('Form controls do not exist'); // Affiche une erreur si les contrôles n'existent pas
+      }
+    } else {
+      console.error('Form is not valid'); // Affiche une erreur si le formulaire n'est pas valide
+    }
+  }
 }
 
 export class SignUpModel  {
-  name: string;
+  txt: string;
   email: string;
   password: string;
 
   constructor() {
+    this.txt = "";
     this.email = "";
-    this.name = "";
     this.password= ""
-  }
+  } // Initialise txt, email et password à une chaîne vide
 }
+
 
 export class LoginModel  {
   email: string;
